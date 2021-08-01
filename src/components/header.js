d922c2c49acc1f97f77432a7ce708b33054a23a1
CharlieWillingale <charliegwillingale@gmail.com>
2021-08-01 19:20:17 +0100

    Initial Commit

--- src/components/header.js
+++ src/components/header.js
@@ -2,41 +2,36 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+
+import "./Layout.css"
+
+
+const Header = ({siteTitle}) => (
+
+    <header>
+
+        <h1>FOOD FROM MY FLAT</h1>
+        <nav>
+            <ul>
+                <li><Link to="/" className='navLinks'>HOME</Link> </li>
+                <li><Link to="/about/" className='navLinks' >ABOUT</Link> </li>
+                <li><Link to="/equipment" className='navLinks'>EQUIPMENT</Link> </li>
+         
+            </ul>
+
+        </nav>
+
+    </header>
+
 )
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
+    Header.propTypes = {
+        siteTitle: PropTypes.string,
+      }
+      
+      Header.defaultProps = {
+        siteTitle: ``,
+      }
+      
+      export default Header
